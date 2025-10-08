c3748992632e25f6bc782bbe2b76d4123a12e9d1
Albert Morlà <albertmorla@protonmail.com>
2025-10-09 00:30:57 +0200

    feat(level3): Add files from Level 3 exercises - Level03_HelloWorld.cs - Level03_MakingsProgrammer.cs - Level03_WhatComesNext.cs

---
+++ Part1_TheBasics/Level03_HelloWorld.cs
@@ -0,0 +1,9 @@
+﻿namespace Part1_TheBasics;
+
+public class Level03_HelloWorld
+{
+    public static void Run()
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
--- Part1_TheBasics/Program.cs
+++ Part1_TheBasics/Program.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Part1_TheBasics;
+
+while (true)
+{
+    Console.Clear();
+    Console.ForegroundColor = ConsoleColor.Cyan;
+    Console.WriteLine("\nSelect an exercise to run (or type 'exit' to quit):");
+    
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.WriteLine("1. Level 3: Hello, World!");
+    Console.WriteLine("2. Level 3: What Comes Next");
+    Console.WriteLine("3. Level 3: The Makings of a Programmer");
+    
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.Write("\nEnter your choice: ");
+    string? choice = Console.ReadLine();
+
+    if (choice?.ToLower() == "exit")
+    {
+        break;
+    }
+
+    try
+    {
+        switch (choice)
+        {
+            case "1":
+                Console.ResetColor();
+                Level03_HelloWorld.Run();
+                break;
+            case "2":
+                Console.ResetColor();
+                Level03_WhatComesNext.Run();
+                break;
+            case "3":
+                Console.ResetColor();
+                Level03_MakingsProgrammer.Run();
+                break;
+            default:
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Invalid selection. Please try again.");
+                Console.ResetColor();
+                break;
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"An error occurred: {ex.Message}");
+        Console.ResetColor();
+    }
+    
+    Console.WriteLine("\nPress any key to return to the menu...");
+    Console.ReadKey();
+}
