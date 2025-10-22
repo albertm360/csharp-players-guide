cae7d1f25e4ba59b07312d75894547495cae78cc
Albert Morlà <albertmorla@protonmail.com>
2025-10-22 18:38:33 +0200

    Add ReparingTheClocktower exercise

---
+++ Part1_TheBasics/Level09_RepairingTheClocktower.cs
@@ -0,0 +1,30 @@
+﻿namespace Part1_TheBasics;
+
+public class Level09_RepairingTheClocktower
+{
+    public static string Pendulum(string prompt)
+    {
+        string tick = "Tick";
+        string tock = "Tock";
+        
+        do
+        {
+            int number;
+            Console.WriteLine(prompt);
+            string? input = Console.ReadLine();
+
+            if (int.TryParse(input, out number) && number >= 0)
+            {
+                return number % 2 == 0 ? tick : tock;
+            }
+            
+            Console.WriteLine("Wrong input. Please enter a non-negative integer.");
+        } while (true);
+    }
+    public static void Run()
+    {
+        const string prompt = "Enter the number: ";
+        string pendulum = Pendulum(prompt);
+        Console.WriteLine(pendulum);
+    }
+}
