8661fdd94e4e4930b5c2fa33c8561eaa8bf1ea7b
Albert Morlà <albertmorla@protonmail.com>
2025-10-29 02:07:14 +0100

    feat(lesson20): Add Properties of Arrows exercise - Moved Arrow and Utils classes to their own files for better organization. - Implemented ArrowP class using propertioes, keeping the old Arrow class for the previous exercise. - Updated menu with new exercise entry.

---
+++ Part2_ObjectOrientedProgramming/Arrow.cs
@@ -0,0 +1,89 @@
+﻿namespace Part2_ObjectOrientedProgramming;
+
+public class Arrow
+{
+    private Arrowhead _arrowhead;
+    private Fletching _fletching;
+    private int _shaft;
+
+    public Arrow(Arrowhead arrowhead, Fletching fletching, int shaft)
+    {
+        _arrowhead = arrowhead;
+        _fletching = fletching;
+        _shaft = shaft;
+    }
+
+    public Arrowhead GetArrowhead()
+    {
+        return _arrowhead;
+    }
+
+    public Fletching GetFletching()
+    {
+        return _fletching;
+    }
+
+    public int GetShaftLength()
+    {
+        return _shaft;
+    }
+
+    public void SetArrowhead(Arrowhead arrowhead)
+    {
+        _arrowhead = arrowhead;
+    }
+
+    public void SetFletching(Fletching fletching)
+    {
+        _fletching = fletching;
+    }
+
+    public void SetShaft(int length)
+    {
+        _shaft = length;
+    }
+
+    private static int GetArrowheadPrice(Arrowhead arrowhead)
+    {
+        switch (arrowhead)
+        {
+            case Arrowhead.Steel:
+                return 10;
+            case Arrowhead.Wood:
+                return 3;
+            case Arrowhead.Obsidian:
+                return 5;
+        }
+
+        throw new ArgumentOutOfRangeException(nameof(arrowhead), "Unexpected arrowhead material.");
+    }
+
+    private static int GetFletchingPrice(Fletching fletching)
+    {
+        switch (fletching)
+        {
+            case Fletching.Plastic:
+                return 10;
+            case Fletching.TurkeyFeathers:
+                return 5;
+            case Fletching.GooseFeathers:
+                return 3;
+        }
+
+        throw new ArgumentOutOfRangeException(nameof(fletching), "Unexpected fletching material.");
+    }
+
+    private static float GetShaftPrice(int shaft)
+    {
+        return shaft * 0.05f;
+    }
+
+    public float GetCost()
+    {
+        float totalCost = 0.0f;
+        totalCost += GetArrowheadPrice(_arrowhead);
+        totalCost += GetFletchingPrice(_fletching);
+        totalCost += GetShaftPrice(_shaft);
+        return totalCost;
+    }
+}
