af2b1b219ed98d33b963157324a3d81a762c19bd
Albert Morlà <albertmorla@protonmail.com>
2025-11-05 20:48:45 +0100

    refactor(Part2): Move const outside of loop

--- Part2_ObjectOrientedProgramming/Exercises/Level24ThePasswordValidator.cs
+++ Part2_ObjectOrientedProgramming/Exercises/Level24ThePasswordValidator.cs
@@ -5,12 +5,12 @@ namespace Part2_ObjectOrientedProgramming.Exercises;
 public class Level24ThePasswordValidator
 {
     private const string Exit = "exit";
+    const string PasswordPrompt = "Enter a password to validate (or type 'exit' to quit): ";
 
     public void Run()
     {
         while (true)
         {
-            const string PasswordPrompt = "Enter a password to validate (or type 'exit' to quit): ";
             string input = Utils.GetValidString(PasswordPrompt);
 
             if (input.ToLower() == Exit) return;
