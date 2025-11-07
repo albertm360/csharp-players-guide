fc0e7e5875b2a72a4fa85d319dc639ffc4612d8d
Albert Morlà <albertmorla@protonmail.com>
2025-11-07 15:42:19 +0100

    feat(Part2): Add Rock-Paper-Scissors Game Implementation - Created Level24RockPaperScissors exercise - Defined HandGesture enum for game moves - Implemented GameLogic class to handle game rules - Developed Player class to represent game participants - Updated Program.cs to integrate the new game functionality

---
+++ Part2_ObjectOrientedProgramming/Models/Player.cs
@@ -0,0 +1,12 @@
+﻿namespace Part2_ObjectOrientedProgramming.Models;
+
+public class Player(string name)
+{
+    public string Name { get; } = name;
+    public int Score { get; private set; }
+
+    public void IncrementScore()
+    {
+        Score++;
+    }
+}
